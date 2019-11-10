d2d784ce0ce80b050575eb5029329891913396e2
ussy <sudo5in5k@gmail.com>
2019-11-10 19:18:13 +0900

    add test codes for viewmodel

--- app/src/test/java/com/example/todoappsandbox/ui/list/TodoViewModelTest.kt
+++ app/src/test/java/com/example/todoappsandbox/ui/list/TodoViewModelTest.kt
@@ -1,10 +1,10 @@
 package com.example.todoappsandbox.ui.list
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.example.todoappsandbox.repository.TodoRepository
 import com.example.todoappsandbox.repository.db.TodoEntity
-import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
@@ -36,15 +36,34 @@ class TodoViewModelTest {
     }
 
     @Test
-    fun verifyObserveTodo() {
-        val observer = mock<Observer<List<TodoEntity>>>()
-        observer.onChanged(any())
-        viewModel.allTodos.observeForever(observer)
+    fun verifyObserveTodoList() {
+        val observer = viewModel.allTodos.testObserver()
 
         val testEntity = TodoEntity(null, "hoge", "piyo")
         `when`(repository.getAllTodos()).thenReturn(listOf(testEntity))
         
         viewModel.loadAllTodos()
         verify(observer).onChanged(listOf(testEntity))
+    }
+
+    @Test
+    fun verifyObserveTodoChecked() {
+        val checkedObserver = viewModel.isCheckedState.testObserver()
+
+        val testEntity = TodoEntity(1, "hoge", "piyo", isChecked = false)
+        `when`(repository.getAllTodos()).thenReturn(listOf(testEntity))
+
+        viewModel.loadAllTodos()
+        viewModel.checkTodo(testEntity)
+        verify(checkedObserver).onChanged(true)
+    }
+
+    /**
+     * make mock observer for liveData
+     */
+    private fun <T> LiveData<T>.testObserver(): Observer<T> {
+        val observer = mock<Observer<T>>()
+        this.observeForever(observer)
+        return observer
     }
 }
