7f7d1e9923b26c307832f3eee408a3780405ec11
ussy <sudo5in5k@gmail.com>
2020-01-22 23:58:43 +0900

    fix delete dialog bug on rotation

--- app/src/main/java/com/example/todoappsandbox/ui/list/DeleteConfirmDialog.kt
+++ app/src/main/java/com/example/todoappsandbox/ui/list/DeleteConfirmDialog.kt
@@ -5,24 +5,35 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.example.todoappsandbox.repository.db.TodoEntity
 
 class DeleteConfirmDialog : DialogFragment() {
 
     var onPositiveListener: DialogInterface.OnClickListener? = null
+    var onNegativeListener: DialogInterface.OnClickListener? = null
+    var entity: TodoEntity? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        if (savedInstanceState != null) dismiss()
         val activity = this.activity
         activity ?: return super.onCreateDialog(savedInstanceState)
 
         val builder = AlertDialog.Builder(activity)
             .setTitle("Delete")
-            .setMessage("This todo was deleted")
+            .setMessage(
+                "Are you sure to delete this todo?\n\nTitle: ${entity?.title
+                    ?: "Nothing"}\nDescription: ${entity?.description ?: "Nothing"}"
+            )
             .setPositiveButton("OK", onPositiveListener)
-            .setNegativeButton("CANCEL", null)
+            .setNegativeButton("CANCEL", onNegativeListener)
+        this.isCancelable = false
         return builder.create()
     }
 
     companion object {
         fun newInstance() = DeleteConfirmDialog()
+        fun newInstance(entity: TodoEntity) = DeleteConfirmDialog().apply {
+            this.entity = entity
+        }
     }
 }
