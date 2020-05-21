3cfae1fb3fde3bda14ddbea1310b7d1c29467257
sudo5in5k <sudo5in5k@gmail.com>
2020-05-21 17:53:24 +0900

    formatter by danger

--- app/src/main/java/com/example/todoappsandbox/data/State.kt
+++ app/src/main/java/com/example/todoappsandbox/data/State.kt
@@ -2,6 +2,6 @@ package com.example.todoappsandbox.data
 
 sealed class State<out T> {
     object Loading : State<Nothing>()
-    data class Success<out T>(val data: List<T>): State<T>()
-    data class Error(val exception: Exception): State<Nothing>()
+    data class Success<out T>(val data: List<T>) : State<T>()
+    data class Error(val exception: Exception) : State<Nothing>()
 }
